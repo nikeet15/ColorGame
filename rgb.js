342cd7457253fb5bcde47eec54d6adf3df3de2d9
nikeet15 <kesharinikeet@gmail.com>
2020-03-30 02:51:58 +0530

    random colors to square boxes provided using JS

--- rgb.js
+++ rgb.js
@@ -1,35 +1,41 @@
 
-function pickrandom()
+var colors = [];
+for (var i = 0; i < 6; i++) 
 {
-	var r= Math.floor(Math.random()*colors.length);
-	return colors[r];
+	var a= pickrandom(256);
+	var b= pickrandom(256);
+	var c= pickrandom(256);
+
+	colors[i]= "rgb("+a+", "+b+", "+c+")";
+	console.log(colors[i]);
 }
 
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-];
+function pickrandom(x)
+{
+	var r= Math.floor(Math.random()*x);
+	return r;
+}
 
 var sqr = document.querySelectorAll(".square");
-var pickedColor = pickrandom();
+var pickedColor = colors[pickrandom(colors.length)];
 var colorDisplay = document.querySelector("#colorDisplay");
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < sqr.length; i++){
+for(var i = 0; i < sqr.length; i++)
 	// add initial colors to squares
 	sqr[i].style.backgroundColor = colors[i];
 
+for(var i = 0; i < sqr.length; i++)
+{
 	//add click listeners to squares
 	sqr[i].addEventListener("click", function() 
 	{
 		var clickedColor = this.style.backgroundColor;
+
 		if(clickedColor === pickedColor) 
 		{
+			console.log(clickedColor+" "+pickedColor);
 			for(var j=0; j<sqr.length; j++)
 			{
 				sqr[j].classList.remove("fade");
