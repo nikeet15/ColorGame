8bb446c90b15504d40e8e7e0489930f2d082ddb9
nikeet15 <kesharinikeet@gmail.com>
2020-03-30 16:24:34 +0530

    added newcolor button and listener

--- rgb.js
+++ rgb.js
@@ -1,30 +1,53 @@
-
 var colors = [];
-for (var i = 0; i < 6; i++) 
-{
-	var a= pickrandom(256);
-	var b= pickrandom(256);
-	var c= pickrandom(256);
-
-	colors[i]= "rgb("+a+", "+b+", "+c+")";
-	console.log(colors[i]);
-}
+var sqr = document.querySelectorAll(".square");
+var colorDisplay = document.querySelector("#colorDisplay");
+var jumbo= document.querySelector(".jumbotron");
+var new_color= document.querySelector("#new_color");
+var stripe= document.querySelector("#stripe");
+var pickedColor;
 
 function pickrandom(x)
-{
+{	// picks a random integer
 	var r= Math.floor(Math.random()*x);
 	return r;
 }
 
-var sqr = document.querySelectorAll(".square");
-var pickedColor = colors[pickrandom(colors.length)];
-var colorDisplay = document.querySelector("#colorDisplay");
+function color_chooser()
+{
+	//makes chooses 6 random colors
+	for (var i = 0; i < 6; i++) 
+	{
+		var a= pickrandom(256);
+		var b= pickrandom(256);
+		var c= pickrandom(256);
 
-colorDisplay.textContent = pickedColor;
+		colors[i]= "rgb("+a+", "+b+", "+c+")";
+		console.log(colors[i]);
+	}
+}
 
-for(var i = 0; i < sqr.length; i++)
+function setcolor()
+{
 	// add initial colors to squares
+	color_chooser();
+
+	// set color code to guess
+	pickedColor = colors[pickrandom(colors.length)];
+	colorDisplay.textContent = pickedColor;
+
+	// make stripe message blank
+	stripe.textContent= "";
+
+	for(var i = 0; i < sqr.length; i++)
 	sqr[i].style.backgroundColor = colors[i];
+}
+
+function buttoncolor(){
+
+	new_color.classList.toggle("buttoncolor")
+}
+
+setcolor();
 
 for(var i = 0; i < sqr.length; i++)
 {
@@ -40,11 +63,22 @@ for(var i = 0; i < sqr.length; i++)
 			{
 				sqr[j].classList.remove("fade");
 				sqr[j].style.backgroundColor= clickedColor;
+				stripe.textContent= "Correct"
 			}
+
+			jumbo.style.backgroundColor= clickedColor;
 		}
 		
 		else 
+		{	
 			this.classList.add("fade");
+			stripe.textContent= "Try Again";
+		}
 	});
 }
+
+
+new_color.addEventListener("click",setcolor);
+new_color.addEventListener("mouseover",buttoncolor);
+new_color.addEventListener("mouseout",buttoncolor);
 
